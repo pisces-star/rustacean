3a4a3f5ac4603f2bc9464c4703f6347d46ba2e59
feiyu <feiyu.wangzf@tuya.com>
2021-03-17 22:40:22 +0800

    finish types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod guessing_game;
 mod by_example;
 mod primitives;
+mod custom_types;
+mod variable_bindings;
+mod types;
 
 fn main() {
     // guessing_game::guessing_game();
     // by_example::by_example();
-    primitives::primitive();
+    // primitives::primitive();
+    // custom_types::custom_types();
+    // variable_bindings::variable_bindings();
+    types::types();
 }
+
 
---
+++ src/types.rs
@@ -0,0 +1,22 @@
+pub fn types(){
+    let decimal = 65.4321_f32;
+
+    // Error! No implicit conversion
+    // let integer: u8 = decimal;
+
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    // let character = decimal as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    unsafe {
+        // 300.0 is 44
+        println!("300.0 is {}", 300.0_f32.to_int_unchecked::<u8>());
+        // -100.0 as u8 is 156
+        println!("-100.0 as u8 is {}", (-100.0_f32).to_int_unchecked::<u8>());
+        // nan as u8 is 0
+        println!("nan as u8 is {}", f32::NAN.to_int_unchecked::<u8>());
+    }
+}
